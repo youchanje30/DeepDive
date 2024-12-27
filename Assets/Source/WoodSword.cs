8b65eeb30983686341c96b695a65d1d38a2a64c9
gmy0150 <gmy0150@naver.com>
2024-12-28 00:56:28 +0900

    검 어느정도 추가, 검생성, UI버튼 처리
    검 어느정도 추가, 검생성, UI버튼 처리

--- Assets/Source/WoodSword.cs
+++ Assets/Source/WoodSword.cs
@@ -9,42 +9,43 @@ public class WoodSword : BaseSword
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !UpgradeEnd)
         {
             SelectUpgrade();
         }
     }
+    
     private void SelectUpgrade()
     {
+        
         Vector3 mousePos = Input.mousePosition;
         Vector3 WorldPos = Camera.main.ScreenToWorldPoint(mousePos);
         WorldPos.z = 0f;
+        RaycastHit2D hit = Physics2D.Raycast(WorldPos, Vector2.zero);
+
         float randomOffsetx = Random.Range(-tolerance, tolerance);
         float randomOffsety = Random.Range(-tolerance, tolerance);
-
-        Vector2 MousePos2D = new Vector2(WorldPos.x + randomOffsetx, WorldPos.y + randomOffsety);
-
-        Vector2 nearPos = Vector2.zero;
-        float maxWeight = float.MinValue;
-        for (int i = 0; i < UpgradeTarget.Length; i++)
+        if(hit.collider != null)
         {
-            float distance = Vector2.Distance(MousePos2D, UpgradeTarget[i]);
-            if (distance > maxDistance )
+            Vector2 MousePos2D = new Vector2(WorldPos.x + randomOffsetx, WorldPos.y + randomOffsety);
+
+            Vector2 nearPos = Vector2.zero;
+            float maxWeight = float.MinValue;
+            for (int i = 0; i < UpgradeTarget.Length; i++)
             {
-                Debug.Log("enter"+ distance);
-                continue;
+                float distance = Vector2.Distance(MousePos2D, UpgradeTarget[i]);
+
+                float weight = distance > 0 ? Mathf.Min(1 / distance, maxValue) : maxValue;
+                if (weight > maxWeight)
+                {
+                    maxWeight = weight;//near Distance weight
+                    nearPos = UpgradeTarget[i];//near Target
+                }
             }
-            float weight = distance > 0 ? Mathf.Min(1 / distance, maxValue) : maxValue;
-            if (weight > maxWeight)
+            if(maxWeight != float.MinValue)
             {
-                maxWeight = weight;//near Distance weight
-                nearPos = UpgradeTarget[i];//near Target
+                Upgrade(maxWeight);
             }
-        }
-        if(maxWeight <= maxDistance && maxWeight != float.MinValue)
-        {
-        Debug.Log($"clcik obj:{nearPos}, weight :{maxWeight},clickpos : {WorldPos}");
-            Upgrade(maxWeight);
         }
     }
 
